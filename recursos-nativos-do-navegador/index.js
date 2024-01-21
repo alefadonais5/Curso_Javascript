ded6a9edacae6e72764751630461a51cdf311190
alefadonais5 <alefadonais5@gmail.com>
2024-01-20 22:12:59 -0300

    Atualização

---
+++ recursos-nativos-do-navegador/index.js
@@ -0,0 +1,12 @@
+let nome = "Alef"
+
+console.log(nome) 
+console.error(nome) //erro//
+console.warn(nome) //alerta
+
+//window.alert(nome) //alerta em mensagem
+//nome = window.prompt("Insira o seu nome") // premite que o usuário insera uma mensagem
+//let confirmacao = window.confirm("Confirma com essa ação") //menagem de confirmação
+//prompt("Opa")
+
+//document
