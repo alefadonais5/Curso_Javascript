7ef5f2cf6c8dee318bec47d62e797ce806555855
alefadonais5 <alefadonais5@gmail.com>
2024-01-20 22:27:50 -0300

    Exercicio 1

---
+++ exercicio-1/index.js
@@ -0,0 +1,11 @@
+let primeiroNome = null
+let sobreNome = null
+let campo_De_Estudo = null
+let ano_De_Nascimento = null
+
+primeiroNome = prompt("Nome: ")
+sobreNome = prompt("Sobrenome: ")
+campo_De_Estudo = prompt ("Qual a sua atuação")
+ano_De_Nascimento = prompt("Ano de Nascimento")
+
+console.log(primeiroNome, sobreNome, campo_De_Estudo, ano_De_Nascimento)
