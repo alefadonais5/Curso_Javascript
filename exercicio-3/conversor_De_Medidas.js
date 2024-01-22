f9f7695e1362834108c100218ac3771b33cc4d51
alefadonais5 <alefadonais5@gmail.com>
2024-01-22 11:43:44 -0300

    Correção

--- exercicio-3/conversor_De_Medidas.js
+++ exercicio-3/conversor_De_Medidas.js
@@ -13,4 +13,34 @@ O programa também deve exibir uma mensagem de “Opção inválida” caso o us
 
 const valor = parseFloat(prompt("Informe o valor em metros: "))
 
-const conversor = prompt("Em qual medida deseja converter? \nAs opções são: \n1. Milímetro (mm) \n2. Centímetro (cm) \n3. Decímetro (dm) \n 4. Decâmetro (dam) \n 5. Hectômetro (hm)\n 6. Quilômetro (km)")
+const conversor = parseFloat(prompt("Em qual medida deseja converter? \nAs opções são: \n1. Milímetro (mm) \n2. Centímetro (cm) \n3. Decímetro (dm)\n4. Decâmetro (dam)\n5. Hectômetro (hm)\n6. Quilômetro (km)"))
+
+switch (conversor) {
+    case 1:
+        let mm = valor*1000
+        alert(valor + " em metros é " + mm + " milímetro")
+        break;
+    case 2:
+        let cm = valor*100
+        alert(valor + " em metros é " + cm + " centímetro")
+        break;
+    case 3:
+        let dm = valor*10
+        alert(valor + " em metros é " + dm + " decímetro")
+        break;
+    case 4:
+        let dam = valor*0.1
+        alert(valor + " em metros é " + dam + "  decâmetro")
+        break;
+    case 5:
+        let hm = valor*0.01
+        alert(valor + " em metros é " + hm + "  hectômetroo")
+        break;
+    case 6:
+        let km = valor*0.001
+        alert(valor + " em metros é " + km + " quilômetro")
+        break;
+    default:
+        alert("Opção inválida! Digite o números das opções(1,2,3,4,5 ou 6")
+        break;
+}
