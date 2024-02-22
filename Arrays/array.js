ea4d5a055d19369a225c32a12bcf08d1af7bc865
Alef Adonais <alefadonais5@gmail.com>
2024-02-22 14:14:51 -0300

    Atualização

---
+++ Arrays/array.js
@@ -0,0 +1,11 @@
+const listaDeCompras = ["Açucar", false]
+console.log(listaDeCompras)
+
+listaDeCompras[0] = "Arroz"
+listaDeCompras[1] = "Feijao"
+listaDeCompras[2] = 7
+listaDeCompras[5] = "Batata"
+
+console.log(listaDeCompras)
+
+console.log(listaDeCompras[10])
